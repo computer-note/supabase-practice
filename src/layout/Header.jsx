1c3614d5aafc04b7c8fa0245befbcecfad3e3c42
computer-note <planomenes@gmail.com>
2024-06-26 18:58:46 +0900

    update (AuthTestPage.jsx) : 헤더에 내비게이션 링크 추가

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
-  return <header className='bg-green-200 h-[100px] '>Header</header>;
+  return (
+    <header className='bg-green-200 h-[100px] '>
+      <navigator className='flex justify-around items-center bg-yellow-200 h-[50%]'>
+        <Link to='/main'>MainPage</Link>
+        <Link to='/login'>MainPage</Link>
+        <Link to='/signup'>MainPage</Link>
+        <Link to='/auth-test'>MainPage</Link>
+      </navigator>
+    </header>
+  );
 }
 
 export default Header;
