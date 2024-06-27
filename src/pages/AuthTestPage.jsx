985881d1779d8356188ccc5dd25be28a94187962
computer-note <planomenes@gmail.com>
2024-06-28 00:44:56 +0900

    update (AuthTestPage.jsx): 현재 로그인 상태를 컴포넌트 상태로 관리 authManager는 로컬스토리지하고만 상호작용하므로 authManager가 반환하는 값이 달라져도 컴포넌트가 리렌더링되지 않음
    authManager가 반환하는 값에 따라 컴포넌트를 리렌더링하기 위해 컴포넌트 상태를 사용

--- src/pages/AuthTestPage.jsx
+++ src/pages/AuthTestPage.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react';
-import authApi from '../supabase/authApi';
 import useInput from './../hooks/useInput';
+import authApi from '../supabase/authApi';
+import authManager from '../manager/authManager';
 
 function AuthTestPage() {
   const [emailInputRef, EmailInputLabelPair] = useInput(
-    'email',
+    '이메일 입력:   ',
     'email-input'
   );
   const [passwordInputRef, PasswordInputLabelPair] = useInput(
-    'password',
+    '패스워드 입력:   ',
     'password-input'
   );
+
   const [outputData, setOutputData] = useState([]);
+  const [isLogin, setIsLogin] = useState(false);
 
   async function handleOnSubmit(e) {
     e.preventDefault();
@@ -21,35 +24,45 @@ function AuthTestPage() {
       passwordInputRef.current.value
     );
 
-    console.log('response ↓');
-    console.dir(response);
-
     if (response.error) {
       setOutputData([
         `${response.error.status} ${response.error.stack}`,
       ]);
-    } else {
-      //TODO: stringify 옵션 공부하기
-      //replacerFn, spacer
-      setOutputData(
-        (
-          JSON.stringify(response.data.user) +
-          JSON.stringify(response.data.session)
-        ).split(',')
-      );
+      return;
     }
+
+    //TODO: stringify 옵션 공부하기: replacerFn, spacer
+    setOutputData(
+      (
+        JSON.stringify(response.data.user) +
+        JSON.stringify(response.data.session)
+      ).split(',')
+    );
+    authManager.setAccessToken(response.data.session.access_token);
+    setIsLogin(true);
     e.target.reset();
+  }
+
+  function handleLogoutButtonClick() {
+    authManager.setLogout();
+    setIsLogin(false);
   }
 
   return (
     <main>
+      <button onClick={handleLogoutButtonClick}>로그아웃하기</button>
+      <p>로그인상태: {`${isLogin}`.toUpperCase()}</p>
       <p>test2@example.com</p>
       <form onSubmit={handleOnSubmit}>
+        <p>로그인하기</p>
         {EmailInputLabelPair}
         {PasswordInputLabelPair}
         <button>로그인</button>
       </form>
       <hr />
+      <div className='h-[100px] bg-amber-200'></div>
+      <p>회원가입</p>
+
       <section className='  w-[600px] bg-green-100'>
         {outputData.map((data, idx) => (
           <ul key={idx} className='break-words'>
